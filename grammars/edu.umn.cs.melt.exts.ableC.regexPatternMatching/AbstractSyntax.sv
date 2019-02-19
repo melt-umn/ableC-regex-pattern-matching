2df9b83e07cc09680b45c849a6963b56cb2a6665
Lucas Kramer <krame505@umn.edu>
2019-02-18 18:58:39 -0600

    Fix mwda error

--- grammars/edu.umn.cs.melt.exts.ableC.regexPatternMatching/AbstractSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.regexPatternMatching/AbstractSyntax.sv
@@ -13,7 +13,8 @@ top::Pattern ::= re::String
   -- ToDo - better pattern would be   ' v @ when (v =~ re) '
   top.pp = text("/" ++ re ++ "/");
   top.decls = [];  --ToDo - look for variables in the pattern
-  top.defs := [];   --ToDo - look for variables in the pattern
+  top.patternDefs := [];   --ToDo - look for variables in the pattern
+  top.defs := [];
   top.errors := []; -- type checking
 
   top.transform =
